1ed81c0394b2731bb3af8999eae65337b95a2859
shivaph <shivaph@hotmail.com>
2024-06-17 22:48:21 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
         'PASSWORD': 'gPTJZYqZJxbKrwGsnxOJDqmIFTjxrCds',
